fc3ccc939aa8656fce74079afc9459e0fd48612d
ParkYoungWoong <thesecon@gmail.com>
2025-04-06 19:15:41 +0900

    영화 상세 정보를 가져올 때 영화가 없을 경우 사용자에게 메시지 표시

--- src/stores/movie.ts
+++ src/stores/movie.ts
@@ -29,9 +29,13 @@ export const useMovieStore = create(
         set({ movies })
         return movies
       },
-      async fetchMovie(id: string): Promise<Movie> {
+      async fetchMovie(id: string): Promise<Movie | null> {
         const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&i=${id}`)
         const movie = await res.json()
+        if (movie.Response === 'False') {
+          set({ movie: null })
+          return null
+        }
         set({ movie })
         return movie
       }
