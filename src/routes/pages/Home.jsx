21c0bb1b6c15c9c5918b83822acde411104426b8
ParkYoungWoong <thesecon@gmail.com>
2025-04-06 17:50:02 +0900

    메인 페이지의 표시 텍스트를 프로젝트에 맞게 수정.

--- src/routes/pages/Home.jsx
+++ src/routes/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="container mx-auto px-3 py-5">
-      <h1 className="text-2xl font-bold">Hi TypeScript!</h1>
+      <h1 className="text-2xl font-bold">Hi JavaScript!</h1>
     </div>
   )
 }
