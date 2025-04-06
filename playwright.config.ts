864be0928bb1926dd6c76518c17f75c2a163cb16
ParkYoungWoong <thesecon@gmail.com>
2025-04-07 00:28:44 +0900

    CI 옵션 수정.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test'
-
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -26,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI ? 'http://127.0.0.1:4173' : 'http://localhost:5173',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,8 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    // url: 'http://localhost:5173',
-    port: process.env.CI ? 4173 : 5173,
+    port: 5173,
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000
   }
