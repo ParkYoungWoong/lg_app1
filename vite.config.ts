6ecb95d8b0f9417a1f2f82aeaf7375c56f6fcbb9
ParkYoungWoong <thesecon@gmail.com>
2025-04-09 08:56:58 +0900

    테스트 무시 옵션 추가.

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['setupTests.tsx'],
+    include: ['src/**/__tests__/**/*.[jt]s?(x)'],
     coverage: {
       provider: 'v8',
       exclude: [
+        'tests/**',
         '**/*.d.ts',
         '**/*.config.[jt]s',
         'src/main.tsx',
